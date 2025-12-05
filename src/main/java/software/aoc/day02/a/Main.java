243f507b37a9bef9710e3f588c2087f9774d394f
Jesus <jesus.tdd03@gmail.com>
2025-12-05 13:15:46 +0000

    Solved day 2a

--- src/main/java/software/aoc/day02/a/Main.java
+++ src/main/java/software/aoc/day02/a/Main.java
@@ -1,7 +1,28 @@
 package software.aoc.day02.a;
 
+import software.aoc.day02.a.io.RemoteRangeLoader;
+import software.aoc.day02.a.model.Range;
+
+import java.util.List;
+
 public class Main {
     static void main() {
+        List<Range> ranges = new RemoteRangeLoader(Main::toRange).loadAll();
+        long total = 0;
+        for (Range range : ranges) {
+            total += range.getRangeStream()
+                    .filter(id -> (id + "").matches("^(\\d+)\\1+$"))
+                    .reduce(0L, Long::sum);
+        }
+        System.out.println("Sum of invalid IDs: " + total);
+    }
 
+    private static Range toRange(String s) {
+        String[] split = s.replaceAll("\n", "").split("-");
+        return new Range(toLong(split[0]), toLong(split[1]));
+    }
+
+    private static long toLong(String s) {
+        return Long.parseLong(s);
     }
 }
