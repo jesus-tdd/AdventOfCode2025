86d2fe7eb274e9e9bf045787cc8244e767e7dbcd
Jesus <jesus.tdd03@gmail.com>
2025-12-05 13:14:58 +0000

    Implemented method getRangeStream

--- src/main/java/software/aoc/day02/a/model/Range.java
+++ src/main/java/software/aoc/day02/a/model/Range.java
@@ -1,4 +1,11 @@
 package software.aoc.day02.a.model;
 
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
 public record Range(long from, long to) {
+
+    public Stream<Long> getRangeStream() {
+        return LongStream.rangeClosed(from, to).boxed();
+    }
 }
