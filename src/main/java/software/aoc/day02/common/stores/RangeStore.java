6a80cdde6438188612b7bd9674231ac3a4deec4c
Jesus <jesus.tdd03@gmail.com>
2025-12-07 12:03:07 +0000

    Implemented methods stream and expandedStream

--- src/main/java/software/aoc/day02/common/stores/RangeStore.java
+++ src/main/java/software/aoc/day02/common/stores/RangeStore.java
@@ -4,6 +4,8 @@ import software.aoc.day02.common.model.Range;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class RangeStore {
     private final List<Range> ranges;
@@ -20,5 +22,13 @@ public class RangeStore {
 
     public void put(Range range) {
         this.ranges.add(range);
+    }
+
+    public Stream<Range> stream() {
+        return this.ranges.stream();
+    }
+
+    public Stream<Long> expandedStream() {
+        return this.stream().flatMap(r -> LongStream.rangeClosed(r.from(), r.to()).boxed());
     }
 }
