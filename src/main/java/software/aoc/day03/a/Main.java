1bd4a321f1f5a15f5b24da86c81e7182bcf4e6f4
Jesus <jesus.tdd03@gmail.com>
2025-12-07 14:08:04 +0000

    Solved day3a

---
+++ src/main/java/software/aoc/day03/a/Main.java
@@ -0,0 +1,15 @@
+package software.aoc.day03.a;
+
+import software.aoc.common.io.RemoteInputLoader;
+import software.aoc.day03.common.io.BankParser;
+import software.aoc.day03.common.model.Bank;
+
+import java.util.List;
+
+public class Main {
+    static void main() {
+        List<Bank> list = new RemoteInputLoader<>(3, BankParser::parse).loadAll();
+        int total = list.stream().map(Bank::getMaxJolts).reduce(0, Integer::sum);
+        System.out.println("Total jolts:" + total);
+    }
+}
