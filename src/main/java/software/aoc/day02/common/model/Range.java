f5f5bd9db4bb533c35a54892113ee314bd12b6a5
Jesus <jesus.tdd03@gmail.com>
2025-12-07 12:03:36 +0000

    Removed methods to keep SRP

--- src/main/java/software/aoc/day02/common/model/Range.java
+++ src/main/java/software/aoc/day02/common/model/Range.java
@@ -1,17 +1,5 @@
 package software.aoc.day02.common.model;
 
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
-
 public record Range(long from, long to) {
 
-    public Stream<Long> getRangeStream() {
-        return LongStream.rangeClosed(from, to).boxed();
-    }
-
-    public long findInvalidIds() {
-        return this.getRangeStream()
-                .filter(id -> (id + "").matches("^(\\d+)\\1$"))
-                .reduce(0L, Long::sum);
-    }
 }
