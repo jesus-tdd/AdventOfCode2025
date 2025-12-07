b5423f0bda86f8816026accea29e05bb205d7972
Jesus <jesus.tdd03@gmail.com>
2025-12-07 12:04:09 +0000

    Solved day2a with the new architecture

--- src/main/java/software/aoc/day02/a/Main.java
+++ src/main/java/software/aoc/day02/a/Main.java
@@ -2,17 +2,27 @@ package software.aoc.day02.a;
 
 import software.aoc.day02.common.io.RemoteRangeLoader;
 import software.aoc.day02.common.model.Range;
+import software.aoc.day02.common.stores.RangeStore;
 
 import java.util.List;
 
 public class Main {
     static void main() {
-        List<Range> ranges = new RemoteRangeLoader(Main::toRange).loadAll();
-        long total = 0;
-        for (Range range : ranges) {
-            total += range.findInvalidIds();
-        }
+        RangeStore store = new RangeStore();
+        store.put(getRanges());
+        Long total = store.expandedStream()
+                          .filter(Main::isInvalid)
+                          .reduce(0L, Long::sum);
+
         System.out.println("Sum of invalid IDs: " + total);
+    }
+
+    private static boolean isInvalid(Long n) {
+        return (n + "").matches("^(\\d+)\\1$");
+    }
+
+    private static List<Range> getRanges() {
+        return new RemoteRangeLoader(Main::toRange).loadAll();
     }
 
     private static Range toRange(String s) {
