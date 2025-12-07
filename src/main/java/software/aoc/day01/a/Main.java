3b9add5a670f1ed7b5a328e70a64c9591001b4f6
Jesus <jesus.tdd03@gmail.com>
2025-12-07 12:12:23 +0000

    Implemented RotationParser

--- src/main/java/software/aoc/day01/a/Main.java
+++ src/main/java/software/aoc/day01/a/Main.java
@@ -1,16 +1,16 @@
 package software.aoc.day01.a;
 
 import software.aoc.day01.common.io.RemoteRotationLoader;
+import software.aoc.day01.common.io.RotationParser;
 import software.aoc.day01.common.model.Dial;
 import software.aoc.day01.common.model.Rotation;
-import software.aoc.day01.common.model.Rotation.Direction;
 
 import java.util.List;
 
 public class Main {
 
     static void main() {
-        List<Rotation> rotations = new RemoteRotationLoader(Main::toRotation).loadAll();
+        List<Rotation> rotations = new RemoteRotationLoader(RotationParser::parse).loadAll();
         Dial dial = new Dial(0, 99, 50);
         int count = 0;
         for (Rotation rotation : rotations) {
@@ -18,17 +18,5 @@ public class Main {
             if (dial.isPointingAt(0)) count++;
         }
         System.out.println("Password: " + count);
-    }
-
-    private static Rotation toRotation(String s) {
-        return new Rotation(toDirection(s.substring(0, 1)), toInt(s.substring(1)));
-    }
-
-    private static Direction toDirection(String s) {
-        return Direction.valueOf(s);
-    }
-
-    private static int toInt(String s) {
-        return Integer.parseInt(s);
     }
 }
---
+++ src/main/java/software/aoc/day01/common/io/RotationParser.java
@@ -0,0 +1,17 @@
+package software.aoc.day01.common.io;
+
+import software.aoc.day01.common.model.Rotation;
+
+public class RotationParser {
+    public static Rotation parse(String s) {
+        return new Rotation(toDirection(s.substring(0, 1)), toInt(s.substring(1)));
+    }
+
+    private static Rotation.Direction toDirection(String s) {
+        return Rotation.Direction.valueOf(s);
+    }
+
+    private static int toInt(String s) {
+        return Integer.parseInt(s);
+    }
+}
