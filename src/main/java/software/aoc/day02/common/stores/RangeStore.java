2778e80920f03bac42a1a3f4d721695687968965
Jesus <jesus.tdd03@gmail.com>
2025-12-07 11:32:59 +0000

    Implemented class RangeStore

---
+++ src/main/java/software/aoc/day02/common/stores/RangeStore.java
@@ -0,0 +1,24 @@
+package software.aoc.day02.common.stores;
+
+import software.aoc.day02.a.model.Range;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RangeStore {
+    private final List<Range> ranges;
+    
+    public RangeStore() {
+        ranges = new ArrayList<>();
+    }
+    
+    public void put(List<Range> ranges) {
+        for (Range r : ranges) {
+            this.put(r);
+        }
+    }
+
+    public void put(Range range) {
+        this.ranges.add(range);
+    }
+}
