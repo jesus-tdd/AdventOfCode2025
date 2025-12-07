dbb84978f240d17ba394d93c504b7aa9927e9ffc
Jesus <jesus.tdd03@gmail.com>
2025-12-07 12:09:57 +0000

    Solved day2b

--- src/main/java/software/aoc/day02/b/Main.java
+++ src/main/java/software/aoc/day02/b/Main.java
@@ -1,7 +1,30 @@
 package software.aoc.day02.b;
 
+import software.aoc.day02.common.io.RangeParser;
+import software.aoc.day02.common.io.RemoteRangeLoader;
+import software.aoc.day02.common.model.Range;
+import software.aoc.day02.common.stores.RangeStore;
+
+import java.util.List;
+
 public class Main {
     static void main() {
+        RangeStore store = new RangeStore();
+        store.put(getRanges());
+        Long total = store.expandedStream()
+                .filter(Main::isInvalid)
+                .reduce(0L, Long::sum);
 
+        System.out.println("Sum of invalid IDs: " + total);
     }
+
+    private static boolean isInvalid(Long n) {
+        return (n + "").matches("^(\\d+)\\1+$");
+    }
+
+    private static List<Range> getRanges() {
+        return new RemoteRangeLoader(RangeParser::parse).loadAll();
+    }
+
+
 }
