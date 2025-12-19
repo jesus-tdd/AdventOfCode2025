cf0e31388ff2d995186d4759475f404a4439de9a
Jesus <jesus.tdd03@gmail.com>
2025-12-19 20:57:29 +0000

    Updated put method

--- src/main/java/software/aoc/day05/common/stores/RangeStore.java
+++ src/main/java/software/aoc/day05/common/stores/RangeStore.java
@@ -4,6 +4,7 @@ import software.aoc.day05.common.model.Range;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class RangeStore {
     private final List<Range> ranges;
@@ -13,7 +14,21 @@ public class RangeStore {
     }
 
     public void put(Range range) {
-        ranges.add(range);
+        List<Range> list = new ArrayList<>(this.getIntersectingRanges(range).toList());
+        this.remove(list);
+        list.add(range);
+        ranges.add(new Range(
+                list.stream().map(Range::from).min(Long::compare).orElseThrow(),
+                list.stream().map(Range::to).max(Long::compare).orElseThrow()
+        ));
+    }
+
+    public void remove(Range range) {
+        ranges.remove(range);
+    }
+
+    public void remove(List<Range> ranges) {
+        ranges.forEach(this::remove);
     }
 
     public void put(List<Range> ranges) {
@@ -25,5 +40,17 @@ public class RangeStore {
             if (range.contains(n)) return true;
         }
         return false;
+    }
+
+    public Stream<Range> getIntersectingRanges(Range range) {
+        return ranges.stream().filter(r -> r.intersects(range));
+    }
+
+    public long size() {
+        return ranges.size();
+    }
+
+    public long totalNumbers() {
+        return ranges.stream().map(Range::size).reduce(0L, Long::sum);
     }
 }
