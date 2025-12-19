5358c360833b2ab1c045f236ea577147c22451bd
Jesus <jesus.tdd03@gmail.com>
2025-12-19 20:56:14 +0000

    Implemented methods size and intersects

--- src/main/java/software/aoc/day05/common/model/Range.java
+++ src/main/java/software/aoc/day05/common/model/Range.java
@@ -4,4 +4,12 @@ public record Range(long from, long to) {
     public boolean contains(long n) {
         return from <= n && n <= to;
     }
+
+    public boolean intersects(Range other) {
+        return !(other.to < this.from || other.from > this.to);
+    }
+
+    public long size() {
+        return to - from + 1;
+    }
 }
