10f8d0637f43954c01967c1ee09ba7fd331a4ebd
Jesus <jesus.tdd03@gmail.com>
2025-12-20 12:42:21 +0000

    Solved day6a

---
+++ src/main/java/software/aoc/day06/a/Main.java
@@ -0,0 +1,15 @@
+package software.aoc.day06.a;
+
+import software.aoc.day06.common.io.OperationLoader;
+import software.aoc.day06.common.io.OperationParser;
+import software.aoc.day06.common.model.Operation;
+
+import java.util.List;
+
+public class Main {
+    static void main() {
+        List<Operation> homework = new OperationLoader(OperationParser::parse).loadAll();
+        long total = homework.stream().map(Operation::apply).reduce(0L, Long::sum);
+        System.out.println("Grand total: " + total);
+    }
+}
