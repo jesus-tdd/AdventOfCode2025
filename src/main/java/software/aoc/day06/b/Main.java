7b1f12d02a67cf43094b7a44e27ed732d8236f39
Jesus <jesus.tdd03@gmail.com>
2025-12-23 12:41:33 +0000

    Solved day6b

---
+++ src/main/java/software/aoc/day06/b/Main.java
@@ -0,0 +1,15 @@
+package software.aoc.day06.b;
+
+import software.aoc.day06.common.io.CephalopodOperationParser;
+import software.aoc.day06.common.io.OperationLoader;
+import software.aoc.day06.common.model.Operation;
+
+import java.util.List;
+
+public class Main {
+    static void main() {
+        List<Operation> homework = new OperationLoader(CephalopodOperationParser::parse).loadAll();
+        long total = homework.stream().map(Operation::apply).reduce(0L, Long::sum);
+        System.out.println("Grand total: " + total);
+    }
+}
