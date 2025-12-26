8eccb79b5c4e6b9433c0598774e5a4cb4ab3e1a7
Jesus <jesus.tdd03@gmail.com>
2025-12-26 12:08:26 +0000

    Solved day7a

---
+++ src/main/java/software/aoc/day07/a/Main.java
@@ -0,0 +1,27 @@
+package software.aoc.day07.a;
+
+import software.aoc.day07.common.io.CellListLoader;
+import software.aoc.day07.common.io.CellListParser;
+import software.aoc.day07.common.model.Cell;
+import software.aoc.day07.common.model.Grid;
+import software.aoc.day07.common.factory.GridBuilder;
+import software.aoc.day07.common.services.GridSimulator;
+
+import java.util.List;
+
+public class Main {
+    static void main() {
+        List<List<Cell>> cells = new CellListLoader(CellListParser::parse).loadAll();
+        Grid grid = getGrid(cells);
+        grid = new GridSimulator(grid).simulate();
+        System.out.println("Total splits: " + grid.getActiveSplitters());
+    }
+
+    private static Grid getGrid(List<List<Cell>> cells) {
+        return new GridBuilder()
+                .rows(cells.size())
+                .columns(cells.getFirst().size())
+                .cells(cells)
+                .build();
+    }
+}
