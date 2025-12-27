4d125433566ee567cfec0d8246eef9baeb1a2164
Jesus <jesus.tdd03@gmail.com>
2025-12-27 08:59:58 +0000

    Tests for day7b

---
+++ src/test/java/Day7BTest.java
@@ -0,0 +1,64 @@
+import org.junit.Test;
+import software.aoc.day07.common.io.CellListParser;
+import software.aoc.day07.common.model.Cell;
+import software.aoc.day07.common.model.Grid;
+import software.aoc.day07.common.factory.GridBuilder;
+import software.aoc.day07.common.services.QuantumGridSimulator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class Day7BTest {
+    @Test
+    public void start_tachyon_beam() {
+        Grid grid = TestGridBuilder.from(
+                ".S.",
+                "..."
+        );
+        assertThat(new QuantumGridSimulator(grid).step().size()).isEqualTo(1);
+    }
+    @Test
+    public void continue_tachyon_beam() {
+        Grid grid = TestGridBuilder.from(
+                ".|.",
+                "..."
+        );
+        assertThat(new QuantumGridSimulator(grid).step().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void split_nothing() {
+        Grid grid = TestGridBuilder.from(
+                "...",
+                ".^.",
+                "..."
+        );
+        assertThat(new QuantumGridSimulator(grid).step().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void split_tachyon_beam() {
+        Grid grid = TestGridBuilder.from(
+                ".|.",
+                ".^.",
+                "..."
+        );
+        assertThat(new QuantumGridSimulator(grid).step().size()).isEqualTo(2);
+    }
+
+    private static class TestGridBuilder {
+        public static Grid from(String... strings) {
+            List<List<Cell>> list = new ArrayList<>();
+            Arrays.stream(strings).map(CellListParser::parse).forEach(list::add);
+
+            return new GridBuilder()
+                    .rows(list.size())
+                    .columns(list.getFirst().size())
+                    .cells(list)
+                    .build();
+        }
+    }
+}
