e19addeb4707238df5fec4dba4499c630145192c
Jesus <jesus.tdd03@gmail.com>
2025-12-30 11:30:51 +0000

    Solved day7b

---
+++ src/main/java/software/aoc/day07/b/Main.java
@@ -0,0 +1,26 @@
+package software.aoc.day07.b;
+
+import software.aoc.day07.common.factory.GridBuilder;
+import software.aoc.day07.common.io.CellListLoader;
+import software.aoc.day07.common.io.CellListParser;
+import software.aoc.day07.common.model.Cell;
+import software.aoc.day07.common.model.Grid;
+import software.aoc.day07.common.services.QuantumGridSimulator;
+
+import java.util.List;
+
+public class Main {
+    static void main() {
+        List<List<Cell>> cells = new CellListLoader(CellListParser::parse).loadAll();
+        long timelines = new QuantumGridSimulator(getGrid(cells)).simulate().countTimelines();
+        System.out.println("Total timelines: " + timelines);
+    }
+
+    private static Grid getGrid(List<List<Cell>> cells) {
+        return new GridBuilder()
+                .rows(cells.size())
+                .columns(cells.getFirst().size())
+                .cells(cells)
+                .build();
+    }
+}
